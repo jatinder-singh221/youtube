244ea3e1e38c5cca850fc3ff9650f14f6cefce72
jatinder-singh221 <jatindersing19@gmail.com>
2021-10-25 15:41:10 +0530

    created view for user to watch video

--- backend/video/urls.py
+++ backend/video/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import creater_video_view_create, creater_video_delete_view, admin_video_view
+from .views import creater_video_view_create, creater_video_delete_view, admin_video_view, user_video_view
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -8,5 +8,6 @@ router.register(r'^', admin_video_view)
 urlpatterns = [
     path('view/',creater_video_view_create.as_view()),
     path('view/<int:pk>/',creater_video_delete_view.as_view()),
+    path('watch/<int:pk>/', user_video_view.as_view()),
     path('videoadmin',include(router.urls) )
 ]
--- backend/video/views.py
+++ backend/video/views.py
@@ -50,14 +50,31 @@ class creater_video_delete_view(GenericAPIView, RetrieveModelMixin, UpdateModelM
         return self.retrieve(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
+        user = self.request.user
+        channel = channel_model.objects.select_related('user').get(user = user.id) 
+
+        if request.data.get('channel') is not None:
+
+            if int(request.data.get('channel')) == channel.id:
+                return self.partial_update(request, *args, **kwargs)
+
+            else:
+                return Response(status = status.HTTP_401_UNAUTHORIZED)
+                
+        else:
+            return self.partial_update(request, *args, **kwargs)
     
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
 
- 
-class user_video_view():
-    pass
+
+class user_video_view(RetrieveModelMixin, GenericAPIView):
+    queryset = video.objects.all()
+    serializer_class = video_serializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
 
 @method_decorator(csrf_protect, name = 'dispatch')
 class admin_video_view(viewsets.ModelViewSet):
